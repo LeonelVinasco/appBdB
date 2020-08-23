8f9a6c805ac8ce1c3b4d486e57e52e7aed81cb99
Leonel <leonelvinasco@gmail.com>
2020-08-23 02:33:05 -0500

    Revert "Configuracion aplicativo"
    This reverts commit e1490682ac2bfb681f8770eb9a09a124cb298d1f.

--- server/index.js
+++ server/index.js
@@ -1,68 +1,28 @@
-const express=require('express')
 const keys = require('./keys')
+const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const {Pool} = require('pg')
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
 
+const {Pool} = require('pg')
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
+    user: keys.POSTGRES_User,
+    host: keys.POSTGRES_Host,
+    database: keys.POSTGRES_Database,
+    password: keys.POSTGRES_Password,
+    port: keys.POSTGRES_Port
 })
 
-//pgClient.on('error', ()=> console.log('Lost PG connection'))
+pgClient.on('error', ()=> console.log('Lost PG connection'))
 
-/* pgClient
-    .query(`CREATE TABLE IF NOT EXISTS employee (
-        employee_number NUMERIC PRIMARY KEY, 
-        fullname VARCHAR(50) NOT NULL, 
-        function VARCHAR(50) NOT NULL, 
-        boss_number NUMERIC)` 
-        )
-    .catch(err => console.log(err))
- */
-/* pgClient
-.query(`CREATE OR REPLACE FUNCTION setboss(boss int,employee int) 
-        RETURNS TABLE (employee_number NUMERIC, boss_number NUMERIC) AS
-            $BODY$
-                UPDATE employee
-                SET boss_number=$1
-                WHERE employee_number=$2
-                RETURNING employee_number, boss_number;
-            $BODY$
-        LANGUAGE sql VOLATILE;` 
-    )
-.catch(err => console.log(err))
-
-pgClient
-.query(`CREATE OR REPLACE FUNCTION employee(
-            employeeNumber NUMERIC, 
-            fullname VARCHAR(50),
-            function VARCHAR(50),
-            bossNumber NUMERIC) 
-        RETURNS TABLE (employee_number NUMERIC) AS
-            $BODY$
-                INSERT INTO employee(employee_number, fullname, function, boss_number)
-                VALUES ($1,$2,$3,$4)
-                RETURNING employee_number;
-            $BODY$
-        LANGUAGE sql VOLATILE;`)
-.catch(err => console.log(err))
-
- */
 app.get('/', async (req, res)=>{
-//const values = await pgClient.query(`SELECT * FROM employees(1,'Deisy Torres', 'Licenciada', 2)`)
-    //console.log(values)
-    res.send('<h1>Prueba BdB</h1>')
-})
+        res.send('<h1>Prueba BdB</h1>')
+    })
 
-app.listen(5000, ()=>{
-    console.log('Escuchando')
-})
+    app.listen(3000, ()=>{
+        console.log('Escuchando')
+    })
--- server/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-    pgUser: process.env.POSTGRES_USER,
-    pgHost: process.env.POSTGRES_HOST,
-    pgDatabase: process.env.POSTGRES_DATABASE,
-    pgPassword: process.env.POSTGRES_PASSWORD,
-    pgPort: process.env.POSTGRES_PORT
-}
