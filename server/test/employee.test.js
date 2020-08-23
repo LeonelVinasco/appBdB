7eca90043fbc6941d2ab943f73f1becaac95d9b9
Leonel <leonelvinasco@gmail.com>
2020-08-23 11:11:23 -0500

    Archivo de configuracion travis CI

---
+++ server/test/employee.test.js
@@ -0,0 +1,47 @@
+const supertest = require("supertest");
+const assert = require('assert');
+const app = require("../index");
+
+describe("GET /", function() {
+    it("it should has status code 200", function(done) {
+      supertest(app)
+        .get("/")
+        .expect(200)
+        .end(function(err, res){
+          if (err) done(err);
+          done();
+        });
+    });
+  });
+
+  describe('POST /employee/add', function() {
+    it('responds with status code 200', function(done) {
+      supertest(app)
+        .post('/employee/add/?fullname="Vanessa Cardozo"&id=2387454565645434&boss=&func="Manager"')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
+    it('responds with status code 200', function(done) {
+      supertest(app)
+        .post('/employee/add/?fullname=&id=&boss=&func="Manager"')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
+    it('responds with status code 200', function(done) {
+      supertest(app)
+        .post('/employee/add/?fullname="Carlos Cardozo"&id=111434&boss=&func="Manager"')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
